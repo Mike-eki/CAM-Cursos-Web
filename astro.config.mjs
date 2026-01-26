dbf747c8544078aa1046db45c8df535ebc85003b
mike-eki <miqueas.moreno2016@gmail.com>
2026-01-26 01:39:46 -0300

    Add Github Actions to deploy every push, and new modal component for the courses page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
+    site: "https://mike-eki.github.io",
     experimental: {
         fonts: [
             {
