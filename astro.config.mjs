b30d4119923eccf53c451c58da74825bf15d4baf
mike-eki <miqueas.moreno2016@gmail.com>
2026-01-26 01:51:16 -0300

    Add configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 
     site: "https://mike-eki.github.io",
+    base: "/CAM-Cursos-Web",
     experimental: {
         fonts: [
             {
